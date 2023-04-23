04ecaa11cb5156c84c2a05213e47e7ca8020ff22
ex1one <vyvf11@gmail.com>
2023-04-23 16:05:22 +0500

    initialization

---
+++ src/bot/bot.ts
@@ -0,0 +1,5 @@
+import { Telegraf } from 'telegraf';
+import { message } from 'telegraf/filters';
+
+const token = '5974355300:AAGxgxNH1VbWh6tqPN4_wx7jkrrrNybFIl8';
+const bot = new Telegraf(token);
---
+++ src/composers/start.bot.ts
@@ -0,0 +1,10 @@
+import { Composer } from 'telegraf';
+
+const composer = new Composer();
+
+composer.command('start', (ctx) => {
+  try {
+  } catch (err) {
+    console.error(err);
+  }
+});
